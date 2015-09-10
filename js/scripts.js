25ccfb86fbd46bc151ebfe8968b49f051f8bc8e6
Justin Scott & Austin Kincaid <student@epicodus.com>
2015-09-10 13:48:50 -0700

    Initial Commit

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+var wordOrder = function(sentence) {
+  
+};
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe("wordOrder", function() {
+  it("will take a single word string and re turn that word and a word count", function() {
+    expect(wordOrder("cheese")).to.equal(["cheese", 1])
+  });
+});
