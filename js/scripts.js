a894f8e4609f8a91c1f820b9b889d2542894c68f
Justin Scott & Austin Kincaid <student@epicodus.com>
2015-09-10 14:19:30 -0700

    Add basic spec and methods to return number of common words.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 var wordOrder = function(sentence) {
-  
+  var wordArray = sentence.split(" ");
+  var foundWords = [];
+
+  wordArray.forEach(function(word) {
+    foundWords.push([word,1]);
+  });
+
+  return foundWords;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,13 @@
 describe("wordOrder", function() {
-  it("will take a single word string and re turn that word and a word count", function() {
-    expect(wordOrder("cheese")).to.equal(["cheese", 1])
+  it("will take a single word string and return that word and a word count", function() {
+    expect(wordOrder("cheese")).to.eql([["cheese", 1]]);
+  });
+
+  it("will take a two word string and return those words and a word count for each", function() {
+    expect(wordOrder("cheese ball")).to.eql([["cheese", 1],["ball", 1]]);
+  });
+
+  it("will take a two word repeat string and return one word and word count", function() {
+    expect(wordOrder("ball ball")).to.eql([["ball", 2]]);
   });
 });
